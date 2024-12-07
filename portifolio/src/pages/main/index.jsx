98c9581042f8ed69c43225caef8c11740841cee9
lucasribeiiroo <lucasribeiiroo@hotmail.com>
2024-12-07 13:58:39 -0300

    Fix: caminho da imagem

--- portifolio/src/pages/main/index.jsx
+++ portifolio/src/pages/main/index.jsx
@@ -85,7 +85,7 @@ const Main = () => {
         </div>
         <div id="image-lucas" className="place-items-center mt-20">
           <img
-            src="../src/assets/img_lucas.svg"
+            src="./src/assets/img_lucas.svg"
             className="w-[550px] h-[330px] absolute fill"
           />
 
